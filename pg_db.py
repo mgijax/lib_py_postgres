cb98d3651de9167b0c486e3406c8c558afc1ff35
Sharon Giannatto <Sharon.Giannatto@jax.org>
2016-12-08 21:42:12 +0000

    b_py_postgres-6-0-7-1

--- pg_db.py
+++ pg_db.py
@@ -3,6 +3,8 @@
 #	MySQL and Postgres interaction) in a manner analagous to our existing
 #	db.py module (used for Sybase interaction)
 #
+# 11/21/2016 - TR12069 setAutoTranslate* to False
+#
 # 10/22/2014	ks/lec
 #	- TR11750/postgres
 #
@@ -52,7 +54,7 @@ sharedDbManager = None
 
 returnAsSybase = True
 
-autoTranslate = True
+autoTranslate = False
 
 # back-end-specific translations
 autoTranslate_be = False
@@ -61,12 +63,12 @@ commandLogFile = None
 
 ###--- Functions ---###
 
-def setAutoTranslate (on = True):
+def setAutoTranslate (on = False):
 	global autoTranslate
 	autoTranslate = on
 	return
 
-def setAutoTranslateBE (on = True):
+def setAutoTranslateBE (on = False):
 	global autoTranslate_be
 	autoTranslate_be = on
 	return
